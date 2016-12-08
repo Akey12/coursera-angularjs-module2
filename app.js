34ac9befeb2ef498aa8ae5077eb056b83a11b1bd
Kitty Chong <kchong@angus-systems.com>
2016-12-08 11:22:07 -0500

    Add in functionality

--- app.js
+++ app.js
@@ -6,20 +6,50 @@
     .controller('AlreadyBoughtController', AlreadyBoughtController)
     .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-  function AlreadyBoughtController(ShoppingListCheckOffService) {
-
-  }
 
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
-  function ToBuyController(ShoppingListCheckOffService){
+  function ToBuyController(ShoppingListCheckOffService) {
+      var toBuy = this;
 
+      toBuy.Items = ShoppingListCheckOffService.ToBuyList();
+
+      toBuy.BoughtItem = function(index) {
+          ShoppingListCheckOffService.BoughtItem(index);
+      }
   }
+
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+      var bought = this;
+
+      bought.Items = ShoppingListCheckOffService.BoughtList();
+  }
+    
 
   function ShoppingListCheckOffService() {
     var service = this;
-    var toBuyItems = [];
-    var boughtItems =  [];
+    var toBuyItems = [{ Name: "Cookies", Quantity: 10 },
+    { Name: "Chips", Quantity: 5 },
+    { Name: "Tomato", Quantity: 3 },
+    { Name: "Apples", Quantity: 5 },
+    { Name: "Oranges", Quantity: 4 }];
+
+    var boughtItems = [];
+
+    service.ToBuyList = function() {
+        return toBuyItems;
+    };
+
+    service.BoughtList = function() {
+        return boughtItems;
+    };
+
+    service.BoughtItem = function (index) {
+        // retrieve single item
+        var removed = toBuyItems.splice(index, 1)[0];
+        boughtItems.push(removed);
+    };
   }
 
 })();
